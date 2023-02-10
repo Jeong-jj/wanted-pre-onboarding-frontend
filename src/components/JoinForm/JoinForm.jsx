15b44d120147ed2ca717889e3956ea56a7cb13a2
JeongJun <rgfdds98@gmail.com>
2023-02-10 18:11:29 +0900

    rename: move Input component to common folder

--- src/components/JoinForm/JoinForm.jsx
+++ src/components/JoinForm/JoinForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import { useForm } from "../../hooks/useForm";
-import { Input } from "../Input/Input";
+import { Input } from "../common/Input/Input";
 import { validate } from "../../utils/validate";
 
 import * as S from "./styles";
