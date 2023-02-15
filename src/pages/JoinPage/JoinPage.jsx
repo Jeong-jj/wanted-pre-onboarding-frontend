86f085d22f2f775e0deec4a77d27a339925c68ba
JeongJun <rgfdds98@gmail.com>
2023-02-15 19:16:56 +0900

    feat: token condition redirect & logout function

--- src/pages/JoinPage/JoinPage.jsx
+++ src/pages/JoinPage/JoinPage.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { authToken } from "utils/authtoken";
+
 import { JoinForm } from "components/JoinForm/JoinForm";
 import { LinkContainer } from "components/common/LinkContainer/LinkContainer";
 
 import * as S from "./styles";
 
 export const JoinPage = () => {
+  const checkToken = authToken.getToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (checkToken) {
+      navigate("/signin");
+    }
+  }, [checkToken, navigate]);
+
   return (
     <S.PageContainer>
       <S.PageTitle>😌가입정보를 입력해주세요.</S.PageTitle>
--- src/pages/TodoPage/TodoPage.jsx
+++ src/pages/TodoPage/TodoPage.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+
+import { authToken } from "utils/authtoken";
 
 import { TodoForm } from "components/Todos/TodoForm/TodoForm";
 import { TodoList } from "components/Todos/TodoList/TodoList";
@@ -6,6 +10,22 @@ import { TodoList } from "components/Todos/TodoList/TodoList";
 import * as S from "./styles";
 
 export const TodoPage = () => {
+  const checkToken = authToken.getToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!checkToken) {
+      navigate("/signin");
+    }
+  }, [checkToken, navigate]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    authToken.setToken("");
+    alert("로그아웃 되었습니다!");
+    navigate("/signin");
+  };
+
   return (
     <S.PageContainer>
       <S.Title>📝 할 일을 정리해보세요! 👀</S.Title>
@@ -14,6 +34,10 @@ export const TodoPage = () => {
         <TodoForm />
         <TodoList />
       </S.Todos>
+
+      <S.LogoutButton onClick={logout}>
+        <FiLogOut size={26} />
+      </S.LogoutButton>
     </S.PageContainer>
   );
 };
